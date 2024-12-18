eb5e36433a82486d21207d7d7135b3dd55ad4a4a
Ivanov Kirill A <kiivanov@ozon.ru>
2024-12-18 16:02:59 +0300

    Change zxing/library to zxing/browser

--- src/components/StreamBarcodeReader.vue
+++ src/components/StreamBarcodeReader.vue
@@ -10,11 +10,11 @@
 
 <script>
 
-import { BrowserBarcodeReader, Exception, DecodeHintType } from "@zxing/library";
+import { BrowserBarcodeReader, Exception, DecodeHintType, BarcodeFormat } from "@zxing/library";
 let hints = new Map();
 hints.set(DecodeHintType.TRY_HARDER, true)
 hints.set(DecodeHintType.ASSUME_GS1, true)
-hints.set(DecodeHintType.PURE_BARCODE, true)
+hints.set(DecodeHintType.POSSIBLE_FORMATS, BarcodeFormat.CODE_128)
 
 export default {
   name: "stream-barcode-reader",
